0dfb680dcd3de2f7d32996b37ff6381f61e1813f
g0coders <0adibcs0@gmail.com>
2021-07-24 14:13:23 +0700

    First Commit Deploy sh

---
+++ precache-manifest.be899920fa407fe900538c06a64e1e3e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc58e2c5dbd89f61060f",
+    "url": "css/app.601d2ada.css"
+  },
+  {
+    "revision": "419c739f6bea4dbdbe9c",
+    "url": "css/chunk-vendors.754be4df.css"
+  },
+  {
+    "revision": "f660760f5cf4ef8083e5fad6ac42fcea",
+    "url": "css/styles.css"
+  },
+  {
+    "revision": "7f8fb266196def25dfff0f44668fdbc6",
+    "url": "img/about.jpg"
+  },
+  {
+    "revision": "70fb014f980a913b4ee9b0b174ea80e5",
+    "url": "img/blob (2).svg"
+  },
+  {
+    "revision": "8018e2770f990bf84e85403e00a5bc7d",
+    "url": "img/blob 1.svg"
+  },
+  {
+    "revision": "ed16d2f92422a3ddd7f65f763113c85b",
+    "url": "img/blob.svg"
+  },
+  {
+    "revision": "95e94a01bdac252bb49e84fb4fc4218a",
+    "url": "img/perfil.png"
+  },
+  {
+    "revision": "59eb2d8fe32ba25454daa3d4922e7d55",
+    "url": "img/portfolio1.jpg"
+  },
+  {
+    "revision": "3dda69e0843e07a10edbfc7af5fd1571",
+    "url": "img/portfolio2.jpg"
+  },
+  {
+    "revision": "f3892552b80e77ecd61bf720a4013806",
+    "url": "img/portfolio3.jpg"
+  },
+  {
+    "revision": "618dfc2338040ae042ea23efadc29b0b",
+    "url": "img/project.png"
+  },
+  {
+    "revision": "50576efd0fd16e48c80d6744372bd6c4",
+    "url": "img/testimonial1.jpg"
+  },
+  {
+    "revision": "8772182ede30e1592a9eb84e4380ee56",
+    "url": "img/testimonial2.jpg"
+  },
+  {
+    "revision": "c62f08d03eb18137987b116b179e4335",
+    "url": "img/testimonial3.jpg"
+  },
+  {
+    "revision": "0d9fc3211d5bd092d0fa57af9fca9671",
+    "url": "index.html"
+  },
+  {
+    "revision": "bc58e2c5dbd89f61060f",
+    "url": "js/app.f0c91dda.js"
+  },
+  {
+    "revision": "419c739f6bea4dbdbe9c",
+    "url": "js/chunk-vendors.c822cdb1.js"
+  },
+  {
+    "revision": "0ef538eb11bd5bee71d209f5580a545f",
+    "url": "js/main.js"
+  },
+  {
+    "revision": "be4d6f4195222addaf474b45325e8dbc",
+    "url": "js/mixitup.min.js"
+  },
+  {
+    "revision": "2de782f668c173b0ca507da99c21a6de",
+    "url": "js/swiper-bundle.min.js"
+  },
+  {
+    "revision": "e0a729580260beb10dc108a4dac17787",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
